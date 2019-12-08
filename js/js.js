cd5f3589c51a1b4ca6f7705a6a81e0171fea15d0
Anna Belikova <bel4onok_spb@mail.ru>
2019-12-08 21:33:59 +0300

    Design

--- js/js.js
+++ js/js.js
@@ -1,3 +1,27 @@
 $('#loginButton').click(function(){
     $('#loginModal').modal('toggle');
 });
+
+$('#buttonDir').click(function(){
+    $('#skateRouting').modal('toggle');
+});
+
+$('#buttonNews').click(function(){
+    $('#newsAdding').modal('toggle');
+});
+
+$('#buttonRoute').click(function(){
+    $('#routeAdding').modal('toggle');
+});
+
+$('#regLink').click(function(){
+    $('#loginModal').modal('hide');
+    $('#regModal').modal('toggle');
+    
+});
+
+$('#passwordLink').click(function(){
+    $('#loginModal').modal('hide');
+    $('#passModal').modal('toggle');s   
+});
+
